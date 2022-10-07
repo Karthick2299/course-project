8bbad88b25a8ec9d8fed090e2bba5e357829d65e
Karthick2299 <106722215+Karthick2299@users.noreply.github.com>
2022-10-07 18:08:42 +0530

    imporved the course project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesModule } from './recipes/recipes.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { LoggingService } from './logging.service';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core.module';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    SharedModule,
+    CoreModule,
+    CommonModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [LoggingService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseAPIKey : 'AIzaSyBYTXxn6DtBoYPe7NmYxdRMREoX_pRmc8I'
 };
